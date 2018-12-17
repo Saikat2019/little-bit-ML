9e976225a04332a604847d532482fb85c48b80a0
saikat <saikatmondal410@gmail.com>
2018-12-17 09:02:12 +0530

    modified 1regression.py

--- tutorials/regression/1regression.py
+++ tutorials/regression/1regression.py
@@ -1,7 +1,34 @@
 import pandas as pd
 import quandl, math
 import numpy as np 
+from sklearn import preprocessing, svm ,cross_validation
+from sklearn.model_selection import cross_validate
+from sklearn.linear_model import LinearRegression
+
 
 df = quandl.get('WIKI/GOOGL')
 df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
-print(df.head())
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
+forecast_col = 'Adj. Close'
+df.fillna(value=-99999, inplace=True)
+forecast_out = int(math.ceil(0.0033 * len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
+
+X = np.array(df.drop(['label'],1))
+y = np.array(df['label'])
+
+X = preprocessing.scale(X)
+
+#X = X[:-forecast_out+1]
+#df.dropna(inplace=True)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+print(len(X),len(Y))
+print(X)
